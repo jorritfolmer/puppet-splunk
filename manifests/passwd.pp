3a5c739392d355a91d4bb5de66461c2d4b80cbf5
Jorrit Folmer <jorrit@nanowolk.nl>
2015-12-31 12:32:14 +0100

    Set admin password for first login.

--- manifests/passwd.pp
+++ manifests/passwd.pp
@@ -2,14 +2,35 @@
 class splunk::passwd ( 
   $admin = $splunk::admin,
   $splunk_home = $splunk::splunk_home,
+  $splunk_os_user = $splunk::splunk_os_user,
 ){
   if $admin != undef {
     $hash  = $admin[hash]
     $fn    = $admin[fn]
     $email = $admin[email]
-    exec { "sed -i -e 's#^:admin:.*$#:admin:$hash::$fn:admin:$email::#g' $splunk_home/etc/passwd":
+    file { "$splunk_home/etc/passwd":
+      ensure => present,
+      owner  => $splunk_os_user,
+      mode   => '0600',
+      content => ':admin:::',
+      replace => 'no',
+    }
+    exec { "set admin passwd":
+      command => "sed -i -e 's#^:admin:.*$#:admin:$hash::$fn:admin:$email::#g' $splunk_home/etc/passwd",
       unless  => "grep -qe '^:admin:$hash' $splunk_home/etc/passwd",
       path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+      require => [
+        Class['splunk::installed'],
+        File["$splunk_home/etc/passwd"],
+      ],
+    }
+    file { "$splunk_home/etc/.ui_login":
+      ensure => present,
+      owner  => $splunk_os_user,
+      mode   => '0600',
+      content => '',
+      replace => 'no',
+      require => Exec['set admin passwd'],
     }
   }
 }
