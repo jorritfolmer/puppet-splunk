7d46395da5b7188add9bfe4c6bfea538c00ef7ac
Jorrit Folmer <jorrit@nanowolk.nl>
2016-01-03 22:04:38 +0100

    Added ds_intermediate parameter

--- manifests/deploymentclient.pp
+++ manifests/deploymentclient.pp
@@ -2,10 +2,11 @@
 class splunk::deploymentclient
 (
   $ds = $splunk::ds,
+  $ds_intermediate = $splunk::ds_intermediate,
   $splunk_home = $splunk::splunk_home
 ){
   if $ds == undef {
-    augeas { "${splunk_home}/etc/system/local/deploymentclient.conf":
+    augeas { "${splunk_home}/etc/system/local/deploymentclient.conf deploymentServer":
       require => Class['splunk::installed'],
       lens    => 'Puppet.lns',
       incl    => "${splunk_home}/etc/system/local/deploymentclient.conf",
@@ -14,12 +15,37 @@ class splunk::deploymentclient
       ],
     }
   } else {
-    augeas { "${splunk_home}/etc/system/local/deploymentclient.conf":
+    augeas { "${splunk_home}/etc/system/local/deploymentclient.conf deploymentServer":
       require => Class['splunk::installed'],
       lens    => 'Puppet.lns',
       incl    => "${splunk_home}/etc/system/local/deploymentclient.conf",
       changes => [
-        "set target-broker:deploymentServer/targetUri ${ds}"
+        "set target-broker:deploymentServer/targetUri ${ds}",
+        'set deployment-client/disabled false',
+      ],
+    }
+  }
+  if $ds_intermediate == undef {
+    augeas { "${splunk_home}/etc/system/local/deploymentclient.conf repositoryLocation":
+      require => Class['splunk::installed'],
+      lens    => 'Puppet.lns',
+      incl    => "${splunk_home}/etc/system/local/deploymentclient.conf",
+      changes => [
+        'rm deployment-client/repositoryLocation',
+        'rm deployment-client/serverRepositoryLocationPolicy',
+        'rm deployment-client/reloadDSOnAppInstall',
+      ],
+    }
+  } else {
+    augeas { "${splunk_home}/etc/system/local/deploymentclient.conf repositoryLocation":
+      require => Class['splunk::installed'],
+      lens    => 'Puppet.lns',
+      incl    => "${splunk_home}/etc/system/local/deploymentclient.conf",
+      changes => [
+        'set deployment-client/disabled false',
+        "set deployment-client/repositoryLocation ${splunk_home}/etc/deployment-apps",
+        'set deployment-client/serverRepositoryLocationPolicy rejectAlways',
+        'set deployment-client/reloadDSOnAppInstall true',
       ],
     }
   }
