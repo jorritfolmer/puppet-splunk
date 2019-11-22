50c4764d1f072f9e0f1f8c8f83cf9b61620a47ab
Florian Dematraz <florian.dematraz@japannext.co.jp>
2019-11-22 17:48:15 +0900

    Site awareness for Forwarders

---
+++ manifests/server/forwarder.pp
@@ -0,0 +1,38 @@
+# vim: ts=2 sw=2 et
+#
+# Copyright (c) 2016-2018 Jorrit Folmer
+#
+
+class splunk::server::forwarder (
+  $splunk_home = $splunk::splunk_home,
+  $splunk_os_user = $splunk::real_splunk_os_user,
+  $splunk_os_group = $splunk::real_splunk_os_group,
+  $splunk_dir_mode = $splunk::real_splunk_dir_mode,
+  $splunk_file_mode = $splunk::real_splunk_file_mode,
+  $splunk_type = $splunk::type,
+  $splunk_app_replace = $splunk::splunk_app_replace,
+  $splunk_app_precedence_dir = $splunk::splunk_app_precedence_dir,
+  $clustering = $splunk::clustering,
+){
+  $splunk_app_name = 'puppet_forwarder'
+  $thissite = $clustering[thissite]
+  if $splunk_type == 'uf' {
+    file { ["${splunk_home}/etc/apps/${splunk_app_name}_base",
+            "${splunk_home}/etc/apps/${splunk_app_name}_base/${splunk_app_precedence_dir}",
+            "${splunk_home}/etc/apps/${splunk_app_name}_base/metadata",]:
+      ensure => directory,
+      owner  => $splunk_os_user,
+      group  => $splunk_os_group,
+      mode   => $splunk_dir_mode,
+    }
+    -> file { "${splunk_home}/etc/apps/${splunk_app_name}_base/${splunk_app_precedence_dir}/server.conf":
+      ensure  => present,
+      owner   => $splunk_os_user,
+      group   => $splunk_os_group,
+      mode    => $splunk_file_mode,
+      replace => $splunk_app_replace,
+      content => template("splunk/${splunk_app_name}_base/local/server.conf"),
+    }
+
+  }
+}
