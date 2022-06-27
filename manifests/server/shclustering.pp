jorritfolmer/puppet-splunk

commit 6e049d43714c898955b6d8e24a8ba612de4f71fd

Puppet.lns cannot parse this kind of key [replication_port://8079]
Replication port may be configured in server.conf as documented on
https://docs.splunk.com/Documentation/Splunk/8.2.6/Indexer/Configurepeerswithserverconf
but the presence of this stanza prevents augeas from being able to parse
the file.

The "Splunk.lns" is available in Puppet 5.5, but comes with a
few caveats regarding its usage

https://github.com/hercules-team/augeas/commit/b64ee52dbcc5c5fdfd31f3a6bf13f33d98f2e809

Lens to parse Splunk configuration. Based on IniFile lens, with few
differences:

* Accepts empty value in entry
* Entries without parent section are children of .anon node

manifests/server/shclustering.pp
@@ -61,10 +61,10 @@ class splunk::server::shclustering (
           # to prevent the SH Deployer from overwriting server specific config
           # directives like mgmt_uri 
           -> augeas { "${splunk_home}/etc/system/local/server.conf/shclustering":
-            lens    => 'Puppet.lns',
+            lens    => 'Splunk.lns',
             incl    => "${splunk_home}/etc/system/local/server.conf",
             changes => [
-              "set shclustering/mgmt_uri https://${::fqdn}:8089",
+              "set target[. = 'shclustering']/mgmt_uri https://${::fqdn}:8089",
             ],
           }
         }

manifests/server/general.pp
@@ -23,10 +23,10 @@ class splunk::server::general (
       # delete pass4SymmKey from [general] in etc/system/local/server.conf,
       # otherwise our pass4SymmKey in the app below will be overruled
       augeas { "${splunk_home}/etc/system/local/server.conf pass4symmkey":
-        lens    => 'Puppet.lns',
+        lens    => 'Splunk.lns',
         incl    => "${splunk_home}/etc/system/local/server.conf",
         changes => [
-          'rm general/pass4SymmKey',
+          'rm target[. = "general"]/pass4SymmKey',
         ],
       }
     }

commit 1405fb584ef05f5a31bcb5df1116889d42a91d9a

Augeas will fail if the section is missing
This is a potential regression introduced in
6e049d43714c898955b6d8e24a8ba612de4f71fd. If the `[shclustering]` is
completely missing from the `server.conf`, this augeas rule will fail to
create it.

Even if it is not a practical regression for a Splunk deployment, it's a
nice to make it complete for the benefit of anyone referring to this
code in the future.

manifests/server/shclustering.pp
@@ -64,6 +64,7 @@ class splunk::server::shclustering (
             lens    => 'Splunk.lns',
             incl    => "${splunk_home}/etc/system/local/server.conf",
             changes => [
+              "set target[. = 'shclustering'] shclustering",
               "set target[. = 'shclustering']/mgmt_uri https://${::fqdn}:8089",
             ],
           }